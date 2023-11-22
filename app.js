b66d7b57b2c836bdf054f249f12c7466416b4266
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2023-11-22 12:05:18 +0800

    changed functionality
    instead of mousedown to do a sketch, it's now changed to mouseover

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 let sketching = document.querySelectorAll(".sketch");
 let reset = document.createElement('div')
 reset.classList.add('reset');
-
 sketching.forEach(sketch => {
-    sketch.addEventListener('mousedown', (e) => {
+    sketch.addEventListener('mouseover', (e) => {
         let target = e.target;
         target.classList.add('sketched')
     })
