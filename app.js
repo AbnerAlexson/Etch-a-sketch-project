b052e7a467669e6d81293cc4b8d1578ef5e96607
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2023-11-24 12:12:40 +0800

    "update project"

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 //variables
 let buttonChangeTiles = document.querySelector('#input > button');
+let black = document.querySelector('.black');
+let eraser = document.querySelector('.eraser');
+let random = document.querySelector('.random');
+let reset = document.querySelector('.reset');
+let sketchMode = document.querySelector('#mode');
 
  function setTiles(tiles) {
     let board = document.querySelector('.board');
@@ -11,15 +16,18 @@ let buttonChangeTiles = document.querySelector('#input > button');
     let numTiles = tiles * tiles;
     for (let i = 0; i < numTiles; i++) {
         let sketchTiles = document.createElement("div");
-        sketchTiles.style.backgroundColor = 'yellow';
-        sketchTiles.addEventListener('mouseover', (e) => {
-            let target = e.target;
-            target.style.backgroundColor = 'black';
-        })
+        sketchTiles.style.backgroundColor = '#fff';
         board.insertAdjacentElement('beforeend', sketchTiles);
+        sketchTiles.addEventListener('mouseover', (e) => {
+            if (click) {
+                e.target.style.backgroundColor = color;
+            };
+        });
     };
+
 };
 
+// function to change the size of tile drawing board
 function modifyTiles(input) {
     if (input < 1 || input > 100) {
         alert('Size are only between 1 and 100.');
@@ -27,11 +35,35 @@ function modifyTiles(input) {
         setTiles(input)
     };
 };
-
-
 buttonChangeTiles.addEventListener('click', () => {
     let inputTiles = document.querySelector('#input > input').value;
     modifyTiles(inputTiles);
 })
 
+// button event listener that changes sketch colour
+let color;
+black.addEventListener('click', () => {
+    color = 'black';
+});
+eraser.addEventListener('click', () => {
+    color = '#fff';
+});
+random.addEventListener('click', () => {
+    let red = Math.floor(Math.random() * 256) + 1;
+    let green = Math.floor(Math.random() * 256) + 1;
+    let blue = Math.floor(Math.random() * 256) + 1;
+    color = `rgb${red}, ${green}, ${blue}`;
+});
 
+//Mode that toggles sketch mode
+let click = false;
+document.querySelector('#content').addEventListener('click', () => {
+    click = !click;
+    if (click) {
+        sketchMode.innerText = "True";
+        sketchMode.style.color = "green";
+    } else {
+        sketchMode.innerText = "False";
+        sketchMode.style.color = "red";
+    };
+});
